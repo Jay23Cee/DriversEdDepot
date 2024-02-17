2e07ef262277f55c57c77b1382ff9ee40d1e5d90
JAYCEE <Jayceexxiii@gmail.com>
2024-02-16 22:19:35 -0800

    Update SEO for About us

--- Data/JSON.js
+++ Data/JSON.js
@@ -4,17 +4,13 @@ export const Data = {
       name: "Home",
       path: "/", // Root path for the homepage
       title: "Top-Rated Online Driving School - Expert Training & Certification"
-    },
-    {
-      name: "Why Choose Us",
-      path: "/why-choose-us", // Descriptive path with a clear intention
-      title: "Experienced Instructors and Flexible Driving Courses"
-    },
-    {
-      name: "FAQs",
-      path: "/frequently-asked-questions", // Full phrase for clarity
-      title: "Comprehensive FAQs on Driving Education & Services"
-    },
+    },{
+      name: "About",
+      path: "/About",
+      title: "About DriversEdDepot.com - Your Partner in Driving Education",
+      description: "Learn more about DriversEdDepot.com, your trusted ally in the journey towards safer driving and comprehensive traffic education."
+    }
+
   ],
   Attributes: [
     {
@@ -54,61 +50,31 @@ export const Data = {
       description: "Our driving courses are optimized for any device, enabling you to learn on the go."
     },
   ],
-  CoursesWeOffer: [
-    {
-      img: "/assets/course2.png",
-      alt: "Comprehensive Driving Courses Icon",
-      name: "Comprehensive Courses",
-      description: "Our wide range of driving courses covers everything from basic to advanced driving skills."
-    },
-    {
-      img: "/assets/course1.png",
-      alt: "Specialized Driving Programs Icon",
-      name: "Specialized Programs",
-      description: "Specialized driving programs tailored to meet individual needs, from teens to commercial drivers."
-    },
-    {
-      img: "/assets/course3.png",
-      alt: "Advanced Driver Training Icon",
-      name: "Advanced Training",
-      description: "Take your driving to the next level with our advanced driver training courses."
-    },
-    {
-      img: "/assets/course4.png",
-      alt: "Teen Driver Education Icon",
-      name: "Teen Education",
-      description: "Our teen driver education programs focus on safety and confidence on the road."
-    },
-  ],
-  faqs: {
-    title: "Driving School Affiliate FAQs - Your Guide to Affordable Traffic School Options",
-    description: "Find answers to your questions about our affiliate service that connects you to affordable traffic school courses.",
-    rows: [
-      {
-        title: "Affiliate Service for Traffic Schools",
-        content: "Drivers Ed Depot is an affiliate service that connects you with top-rated low-budget traffic schools. We help you find the right traffic school to meet your requirements."
-      },
-      {
-        title: "Selecting a Traffic School",
-        content: "With our affiliate service, we provide information and direct links to high-quality, affordable traffic schools. This helps you make an informed decision without the need for browsing."
-      },
-      {
-        title: "Benefits of Using Our Affiliate Service",
-        content: "Our service simplifies your search for budget-friendly traffic schools, providing direct access to reputable courses. This ensures a seamless experience in finding the right course for you."
-      },
-      {
-        title: "Process of Signing Up",
-        content: "After selecting a traffic school from our recommendations, you will be redirected to their website to complete the signup process for your chosen course."
-      },
-      {
-        title: "Quality Assurance of Recommended Schools",
-        content: "We ensure that all traffic schools we affiliate with are certified and reputable, offering high-quality education for effective learning."
-      },
-      {
-        title: "Assistance in Choosing the Right School",
-        content: "While our service guides you to various traffic school options, the final decision rests with you. We provide the necessary information to help you choose a school that fits your specific needs."
-      },
-    ],
-  },
-  
+
+  SEO: {
+    About: {
+      title: "About DriversEdDepot.com - Your Partner in Driving Education",
+      description: "Discover the mission, vision, and values of DriversEdDepot.com. Learn how we connect learners with the best online traffic schools tailored to their needs, promoting safer driving and comprehensive traffic education.",
+      keywords: "drivers education, traffic school, safe driving, online education, driving safety, affiliate marketing for traffic schools, budget-friendly traffic education",
+      images: [
+        {
+          src: "/assets/welcome-to-driverseddepot-header.png",
+          alt: "Welcome to DriversEdDepot.com",
+        },
+        {
+          src: "/assets/our-mission-driverseddepot.png",
+          alt: "Our Mission at DriversEdDepot.com",
+        },
+        {
+          src: "/assets/what-we-offer-driverseddepot.png",
+          alt: "What We Offer - Online Traffic Schools",
+        },
+        {
+          src: "/assets/our-commitment-driverseddepot.png",
+          alt: "Our Commitment to Road Safety",
+        }
+      ],
+      url: "/About"
+    }
+  }
 };
